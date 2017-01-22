7a5884dd4a2ae31980d713af4c8ce0d747055887
Kevin Ball <kmball11@gmail.com>
2017-01-21 16:20:05 -0800

    Basic top bar nav for cd page

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -7,8 +7,9 @@ class ZipcodesController < ApplicationController
   def search
     @zipcode = Zipcode.find_by_code(params[:code])
     if @zipcode
-      redirect_to "/#{@zipcode.state.slug}/#{'%2d' % @zipcode.congressional_district.number}"
+      redirect_to "/#{@zipcode.state.slug}/#{'%2d' % @zipcode.congressional_district.number}?zipcode=#{@zipcode.code}"
     else
+      @zipcode = Zipcode.new
       flash[:notice] = "Zipcode not found, try again"
       render :index
     end
